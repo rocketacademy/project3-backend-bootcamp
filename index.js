5bd708abc3efd0fa421e2ea119227beb41413e63
adamvader <adamonglt@hotmail.com>
2022-11-17 18:03:16 +0800

    sequelize set up

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import express from "express";
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
 
-const PORT = 3000;
+//import DB
+const db = require("./db/models/index");
+
+const PORT = process.env.PORT;
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
