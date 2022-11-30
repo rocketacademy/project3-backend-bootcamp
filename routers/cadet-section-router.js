f6c2aee2df283a487fcca9a317ee6e856b1c8d11
weiyu95 <weiyu95@gmail.com>
2022-11-30 22:19:31 +0800

    working cadet-section controller & router, able to query individual cadet-section data

---
+++ routers/cadet-section-router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+class CadetSectionRouter {
+  constructor(controller) {
+    this.controller = controller;
+  }
+  routes() {
+    // we will insert routes into here later on
+    router.get("/", this.controller.getAll.bind(this.controller));
+    router.post("/", this.controller.insertOne.bind(this.controller));
+    router.get("/complete-status", this.controller.getOne.bind(this.controller));
+
+    return router;
+  }
+}
+
+module.exports = CadetSectionRouter;
