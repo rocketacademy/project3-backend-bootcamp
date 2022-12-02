bb057a294ef461d862823bfd559dbc6b933206f1
Jermaine-ye <jer.jmye@gmail.com>
2022-12-02 15:03:48 +0800

    additional chapter router n controller

--- controllers/chaptersController.js
+++ controllers/chaptersController.js
@@ -1,8 +1,20 @@
-const BaseController = require("./baseController");
+const BaseController = require('./baseController');
 
 class ChaptersController extends BaseController {
   constructor(model) {
     super(model);
+  }
+  async getAllChapters(req, res) {
+    const { sectionId } = req.query;
+    try {
+      const { count, rows } = await this.model.findAndCountAll({
+        where: { sectionId: sectionId },
+      });
+      console.log(count);
+      return res.json(rows);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
   }
 
   /** if a method in this extended class AND the base class has the same name, the one in the extended class will run over the base method */
