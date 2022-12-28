9a76123020eb4ee80d01f05b47beffc4b6811d5e
Jermaine-ye <jer.jmye@gmail.com>
2022-12-28 14:54:31 +0800

    added cadetChapter routers and controllers

--- db/seeders/20221123133422-seed-cadet-sections.js
+++ db/seeders/20221123133422-seed-cadet-sections.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
-      "cadetSections",
+      'cadetSections',
       [
         {
           cadetId: 1,
@@ -21,6 +21,34 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
+          cadetId: 1,
+          sectionId: 3,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 1,
+          sectionId: 4,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 1,
+          sectionId: 5,
+          completed: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 1,
+          sectionId: 9,
+          completed: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
           cadetId: 2,
           sectionId: 1,
           completed: true,
@@ -28,8 +56,78 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
+          cadetId: 2,
+          sectionId: 2,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 2,
+          sectionId: 3,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 2,
+          sectionId: 4,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 2,
+          sectionId: 5,
+          completed: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 2,
+          sectionId: 9,
+          completed: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
           cadetId: 3,
           sectionId: 1,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 3,
+          sectionId: 2,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 3,
+          sectionId: 3,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 3,
+          sectionId: 4,
+          completed: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 3,
+          sectionId: 5,
+          completed: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          cadetId: 3,
+          sectionId: 9,
           completed: false,
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -40,6 +138,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("cadetSections", null, {});
+    await queryInterface.bulkDelete('cadetSections', null, {});
   },
 };
