9d521ac458f1182eee812ab4b278220861f47b81
peanutyabing <sherry.liuyabing@gmail.com>
2023-04-11 10:59:37 +0800

    Create database files, config, and folders

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-import express from "express";
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// importing Routers
+
+// importing Controllers
+
+// importing DB
+const db = require("./db/models/index.js");
+
+// initializing Controllers -> note the lowercase for the first word
+
+// inittializing Routers
+
+// using the routers
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
