45d1ccf35680ebc14efba887223cc54cc5c31a27
estney <esther.pxy@gmail.com>
2024-02-05 17:33:48 +0800

    created base. import cors, dotenv, postgres, seq. uppdated index

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-import express from "express";
+//Enable CORS access to this server
+const cors = require("cors");
 
-const PORT = 3000;
+//import express to read json
+const express = require("express");
+
+// import env file
+require("dotenv").config();
+
+//-----------import auth0 middleware
+
+//-----------initializing Controllers, lowercase for the key
+
+//-----------initializing controllers
+
+//-----------importing DB
+// const db = require("./db/models/index");
+// const {} = db;
+
+//port and express
+const PORT = process.env.PORT;
 const app = express();
+
+// Enable CORS access to this server
+app.use(cors());
+
+// Enable reading JSON request bodies
+app.use(express.json());
+
+// <----- ROUTERS ----->
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
