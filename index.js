80f659a8ff281ef58b89d346e62b26a25eb451f7
jasperteo <jaspertzj@outlook.sg>
2024-02-06 12:01:11 +0800

    changed index.js

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
+const { auth } = require("express-oauth2-jwt-bearer");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 const app = express();
+
+// importing Routers
+const ListingsRouter = require("./routers/listingsRouter");
+
+// importing Controllers
+const ListingsController = require("./controllers/listingsController");
+
+// importing DB
+const db = require("./db/models/index");
+const { listing, user } = db;
+
+const checkJwt = auth({
+  audience: process.env.JWT_AUDIENCE,
+  issuerBaseURL: process.env.JWT_ISSUER_BASE_URL,
+});
+
+// initializing Controllers -> note the lowercase for the first word
+const listingsController = new ListingsController(listing, user);
+
+// inittializing Routers
+const listingsRouter = new ListingsRouter(
+  listingsController,
+  checkJwt
+).routes();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.use(cors());
+app.use(express.json());
+app.use("/listings", listingsRouter);
+
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
 });
+
+// server.listen(PORT, () => {
+//   console.log(`Express app listening on port ${PORT}!`);
+// });
