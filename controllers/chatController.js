0fa0cb4f9f01ea89ec6908c7e95799afce49dda2
IffahA <iffah.ada@gmail.com>
2024-02-12 12:31:42 +0800

    Added chat controller/router

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,38 +1,3 @@
-// import category from "./models/category.js";
-// import chat_image from "./models/chat_image.js";
-// import chatroom_message from "./models/chatroom_message.js";
-// import chatroom from "./models/chatroom.js";
-// import index from "./models/index.js";
-// import like from "./models/like.js";
-// import listing_image from "./models/listing_image.js";
-// import listing from "./models/listing.js";
-// import order from "./models/order.js";
-// import review from "./models/review.js";
-// import user from "./models/user.js";
-
-// const getAllChat = (req, res) => {
-//   res.send("All chat");
-// };
-
-// const getSpecificChat = (req, res) => {
-//   res.send("Specific chat");
-// };
-
-// const postMessage = (req, res) => {
-//   res.send("Send message");
-// };
-
-// const deleteMessage = (req, res) => {
-//   res.send("Delete message");
-// };
-
-// export default {
-//   getAllChat,
-//   getSpecificChat,
-//   postMessage,
-//   deleteMessage,
-// };
-
 const { Sequelize } = require("../models");
 const BaseController = require("./baseController");
 
@@ -43,18 +8,18 @@ class ChatController extends BaseController {
   }
 
   //GET ALL CHATS WHERE USER IS EITHER OWNER OF LISTING OR POTENTIAL BUYER
-  async getAll(req, res) {
-    const { userId } = req.body;
-    try {
-      const output = await this.model.findAll({
-        where: { potential_buyer_id: userId },
-      });
-      console.log("HELLO WORLD");
-      return res.send("HELLO WORLD");
-    } catch (err) {
-      return res.status(400).json({ error: true, msg: err });
-    }
-  }
+  // async getAll(req, res) {
+  //   const { userId } = req.body;
+  //   try {
+  //     const output = await this.model.findAll({
+  //       where: { potential_buyer_id: userId },
+  //     });
+  //     console.log("HELLO WORLD");
+  //     return res.send("HELLO WORLD");
+  //   } catch (err) {
+  //     return res.status(400).json({ error: true, msg: err });
+  //   }
+  // }
 
   async createMessage(req, res) {
     const { chatroomId, comment, sender } = req.body;
--- routers/chatRouter.js
+++ routers/chatRouter.js
@@ -1,28 +1,3 @@
-// import express from "express";
-// const chatRouter = express.Router();
-
-// // GET - all chat
-// chatRouter.get("/chat", (req, res) => {
-//   res.send("All chat");
-// });
-
-// // GET - specific chat
-// chatRouter.get("/chat/chatroom", (req, res) => {
-//   res.send("Specific chat");
-// });
-
-// // POST - message
-// chatRouter.post("/chat/message", (req, res) => {
-//   res.send("Send message");
-// });
-
-// // DELETE - message
-// chatRouter.delete("/chat/message", (req, res) => {
-//   res.send("Delete message");
-// });
-
-// export default chatRouter;
-
 const express = require("express");
 const router = express.Router();
 
