5e2f1c5916e887b8ee8c577ccd36f11838dcc3ab
Mystjerne <danaliewqie@gmail.com>
2024-02-17 02:56:30 +0800

    Update Employer Controller
    Add joblistings to employer controller with benefits.

--- controllers/EmployerController.js
+++ controllers/EmployerController.js
@@ -33,7 +33,17 @@ class EmployerController extends BaseController {
     const { firstName, lastName, companyName, email, photo, description } =
       req.body;
     const { employerId } = req.params;
+    //if employerID /= in the list of employers, should throw error and not be able to update
+
     try {
+      let employer_data = await this.model.findOne({
+        where: { id: employerId },
+      });
+      if (employer_data == null) {
+        throw new Error(
+          "Employer you are trying to edit does not exist in the database."
+        );
+      }
       await this.model.update(
         {
           firstName: firstName,
@@ -52,7 +62,7 @@ class EmployerController extends BaseController {
       const output = await this.model.findAll();
       return res.json(output);
     } catch (err) {
-      return res.status(400).json({ error: true, msg: err });
+      return res.status(400).json({ error: true, msg: err.message });
     }
   }
 
@@ -68,6 +78,8 @@ class EmployerController extends BaseController {
       applicationStartDate,
       applicationEndDate,
     } = req.body;
+
+    console.log(req.body);
     try {
       //tag to employerID
       const newJobListing = await this.jobListingModel.create({
@@ -80,7 +92,32 @@ class EmployerController extends BaseController {
         employerId: employerId,
       });
       // Respond with the new work experience
-      return res.json(newJobListing);
+      {
+        const { employerId } = req.params;
+        const { benefit1, benefit2, benefit3 } = req.body;
+
+        try {
+          // Check if the talent exists
+          console.log("req.body", req.body);
+          const employer = await this.model.findByPk(employerId);
+          if (!employer) {
+            return res
+              .status(404)
+              .json({ error: true, msg: "Employer not found" });
+          }
+          // Create a new benefit and associate it with the talent
+          const addJobListingBenefit = await this.benefitModel.findAll({
+            where: {
+              id: [benefit1, benefit2, benefit3],
+            },
+          });
+          await newJobListing.setBenefits(addJobListingBenefit);
+          // Respond with the newly created benefit
+          return res.json(addJobListingBenefit);
+        } catch (err) {
+          return res.status(400).json({ error: true, msg: err });
+        }
+      }
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
