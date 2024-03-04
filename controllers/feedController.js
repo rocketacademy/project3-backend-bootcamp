12a5200daea00a418ddf69dc76666b37c211c39c
phyllispp <hongyu.pann@gmail.com>
2024-03-04 18:00:48 -0500

    added feed and category controllers and routers

---
+++ controllers/feedController.js
@@ -0,0 +1,10 @@
+const BaseController = require("./baseController");
+
+class FeedController extends BaseController {
+  constructor(feedmodel, feedLikeModel, feedReviewModel) {
+    super(feedmodel);
+    this.feedLikeModel = feedLikeModel;
+    this.feedReviewModel = feedReviewModel;
+  }
+}
+module.exports = FeedController;
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 //importing routers
 const UserRouter = require("./routers/userRouter.js");
-const SellerRouter = require("./routers/sellerRouter.js");
+const CategoryRouter = require("./routers/categoryRouter.js");
 const OrderRouter = require("./routers/orderRouter.js");
 const NotificationRouter = require("./routers/notificationRouter.js");
 const FeedRouter = require("./routers/feedRouter.js");
@@ -15,7 +15,7 @@ const BasketRouter = require("./routers/basketRouter.js");
 
 //importing controllers
 const UserController = require("./controllers/userController.js");
-const SellerController = require("./controllers/sellerController.js");
+const CategoryController = require("./controllers/sellerController.js");
 const OrderController = require("./controllers/orderController.js");
 const NotificationController = require("./controllers/notificationController.js");
 const FeedController = require("./controllers/feedController.js");
@@ -42,11 +42,11 @@ const {
 
 //initializing controllers
 const userController = new UserController(user);
-const sellerController = new SellerController(
+const categoryController = new CategoryController(
+  category,
   seller,
   sellerLike,
-  sellerReview,
-  category
+  sellerReview
 );
 const orderController = new OrderController(order, orderedItem);
 const notificationController = new NotificationController(notification);
@@ -56,7 +56,7 @@ const basketController = new BasketController(basket);
 
 //initializing routers
 const userRouter = new UserRouter(userController).routes();
-const sellerRouter = new SellerRouter(sellerController).routes();
+const categoryRouter = new CategoryRouter(categoryController).routes();
 const orderRouter = new OrderRouter(orderController).routes();
 const notificationRouter = new NotificationRouter(
   notificationController
@@ -68,7 +68,7 @@ const basketRouter = new BasketRouter(basketController).routes();
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
-app.use("/seller", sellerRouter);
+app.use("/category", categoryRouter);
 app.use("/order", orderRouter);
 app.use("/notification", notificationRouter);
 app.use("/feed", feedRouter);
