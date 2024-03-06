43c1d27505ffe63372b5f2a908eb9ea086779cb3
phyllispp <hongyu.pann@gmail.com>
2024-03-05 20:12:04 -0500

    added user seeder data

---
+++ db/seeders/20240306005158-seed-users.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          username: "PhyllisP",
+          email: "hpan@gmail.com",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
