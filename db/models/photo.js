1e70e441f8b0755498e14fbb27c8b064d3bcf587
qiannnly <qianlinggg@gmail.com>
2024-03-20 14:03:22 +0800

    specified foreign key references

--- db/models/photo.js
+++ db/models/photo.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class photo extends Model {
     /**
@@ -10,17 +8,26 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-         this.belongsTo(models.book)
+      this.belongsTo(models.book);
     }
   }
-  photo.init({
-    photo_url: DataTypes.STRING,
-    book_id: DataTypes.INTEGER,
-    index: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'photo',
-    underscored: true,
-  });
+  photo.init(
+    {
+      photo_url: DataTypes.STRING,
+      book_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "book",
+          key: "id",
+        },
+      },
+      index: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "photo",
+      underscored: true,
+    }
+  );
   return photo;
 };
