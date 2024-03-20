6367fb86780489ebb30f7d0108c2de588b39332e
qiannnly <qianlinggg@gmail.com>
2024-03-20 13:48:47 +0800

    added association with donation table

--- db/models/request.js
+++ db/models/request.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.user, { as: "bene" });
+      this.belongsTo(models.donation);
     }
   }
   request.init(
