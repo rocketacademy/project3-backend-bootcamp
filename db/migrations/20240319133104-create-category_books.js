46d48ea8af13083ef6a15478620115de6f647d92
qiannnly <qianlinggg@gmail.com>
2024-03-20 14:04:42 +0800

    created migration file for category_books junction table

---
+++ db/migrations/20240319133104-create-category_books.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("category_books", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "categories",
+          key: "id",
+        },
+      },
+      book: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "books",
+          key: "id",
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    queryInterface.dropTable("category_books");
+  },
+};
