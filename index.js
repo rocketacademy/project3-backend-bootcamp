ec244e5554aed266b788b096667fc7585b0c4dcb
qiannnly <qianlinggg@gmail.com>
2024-03-20 14:47:03 +0800

    added books router and controller

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ require("dotenv").config();
 
 // importing Routers
 const UsersRouter = require("./routers/usersRouter");
+const BooksRouter = require("./routers/booksRouter");
 
 // importing Controllers
 const UsersController = require("./controllers/usersController");
+const BooksController = require("./controllers/booksController");
 
 //importing DB
 const db = require("./db/models/index");
-const { user } = db;
+const { user, book } = db;
 
 // Initializing Controllers
 const userController = new UsersController(user);
+const bookController = new BooksController(book);
 
 // Initializing Routers
 const usersRouter = new UsersRouter(userController);
+const booksRouter = new BooksRouter(bookController);
 
 const PORT = 3000;
 const app = express();
@@ -29,6 +33,7 @@ app.use(express.json());
 
 // Enable and use usersRouter
 app.use("/users", usersRouter.routes());
+app.use("/books", booksRouter.routes());
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
