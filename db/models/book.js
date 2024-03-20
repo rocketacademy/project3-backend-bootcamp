0a17b4026899f544f9fa901936b9722ec5473919
qiannnly <qianlinggg@gmail.com>
2024-03-20 13:21:54 +0800

    added associations with photo and donation tables

--- db/models/book.js
+++ db/models/book.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class book extends Model {
     /**
@@ -10,20 +8,24 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.photo);
+      this.hasMany(models.donation);
     }
   }
-  book.init({
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    released_year: DataTypes.INTEGER,
-    condition: DataTypes.STRING,
-    review: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'book',
-    underscored: true,
-  });
+  book.init(
+    {
+      title: DataTypes.STRING,
+      author: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      released_year: DataTypes.INTEGER,
+      condition: DataTypes.STRING,
+      review: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "book",
+      underscored: true,
+    }
+  );
   return book;
 };
